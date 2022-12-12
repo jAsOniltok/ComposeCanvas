19e29ff82ce0b59844df722379dc6e55626f38e5
jAsOn <macleodios@hoogi.com>
2022-12-12 18:47:00 +0900

    커밋 테스트2

--- app/src/main/java/net/niltok/composecanvas/MainActivity.kt
+++ app/src/main/java/net/niltok/composecanvas/MainActivity.kt
@@ -153,7 +153,6 @@ fun MyFace() {
                 width = 3.dp.toPx()
             )
         )
-
         drawLine(
             color = Color.Black,
             start = Offset(center.x , center.y - 210f),
