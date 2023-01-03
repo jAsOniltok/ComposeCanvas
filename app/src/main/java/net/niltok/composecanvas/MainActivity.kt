7748f5364cb237725f2441dbcdc874987638dbb3
jAsOn <labanmacleod@gmail.com>
2023-01-03 20:36:02 +0900

    end MyCircle

--- app/src/main/java/net/niltok/composecanvas/MainActivity.kt
+++ app/src/main/java/net/niltok/composecanvas/MainActivity.kt
@@ -180,14 +180,13 @@ fun MyFace() {
 }
 
 @Composable
-fun MyCircle(){
+fun MyCircle() {
     Canvas(
         modifier = Modifier
             .background(color = Color.LightGray)
             .fillMaxSize()
     ) {
         val 반지름 = 400f
-        val 라디안 = 57f
 
         drawCircle(
             brush = Brush.radialGradient(
@@ -201,19 +200,7 @@ fun MyCircle(){
             radius = 20f,
             center = center
         )
-       /* drawCircle(
-            brush = Brush.radialGradient(
-                colors = listOf(
-                    Color.Red,
-                    Color.Yellow
-                ),
-                center = Offset(center.x + 반지름, center.y),
-                radius = 20f
-            ),
-            radius = 20f,
-            center = Offset(center.x + 반지름, center.y)
-        )*/
-      /*  for(i in 0 .. 반지름.toInt()) {
+        for (각도 in 0..360) {
             drawCircle(
                 brush = Brush.radialGradient(
                     colors = listOf(
@@ -224,36 +211,11 @@ fun MyCircle(){
                     radius = 20f
                 ),
                 radius = 20f,
-                center = Offset(center.x + cos(0f) * i.toFloat(), center.y + sin(0f) * i.toFloat() )
+                center = Offset(
+                    center.x + cos(Math.toRadians(각도.toDouble()).toFloat()) * 반지름,
+                    center.y + sin(Math.toRadians(각도.toDouble()).toFloat()) * 반지름
+                )
             )
         }
-        for(i in 0 .. 반지름.toInt()) {
-            drawCircle(
-                brush = Brush.radialGradient(
-                    colors = listOf(
-                        Color.Red,
-                        Color.Yellow
-                    ),
-                    center = center,
-                    radius = 20f
-                ),
-                radius = 20f,
-                center = Offset(center.x + cos(1f) * i.toFloat(), center.y + sin(1f) * i.toFloat() )
-            )
-        }
-        for(i in 0 .. Math.toDegrees(1.0).toInt()) {
-            drawCircle(
-                brush = Brush.radialGradient(
-                    colors = listOf(
-                        Color.Red,
-                        Color.Yellow
-                    ),
-                    center = center,
-                    radius = 20f
-                ),
-                radius = 20f,
-                center = Offset(center.x + cos(Math.toRadians(i.toDouble()).toFloat()) * 반지름, center.y + sin(Math.toRadians(i.toDouble()).toFloat()) * 반지름 )
-            )
-        }*/
     }
 }
